999e5e9949b58e7c1c279d28ebc0057525b6084a
yaqubmw <yaqubmw@gmail.com>
2024-05-27 10:41:30 +0700

    update: main font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className={inter.className}>
         <Main />
         <NextScript />
       </body>
